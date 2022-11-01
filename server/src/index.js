dd112f2c7e39222a4794d31bb55cb232ae9e4b9e
Gabriel <gkqsj@cin.ufpe.br>
2022-11-01 17:46:11 -0300

    Added mongoDB connection to server

--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const routes = require('./routes')
 const cors = require('cors')
+const mongoose = require('mongoose')
+const { urlencoded } = require('express')
 
 const app = express()
 
@@ -8,4 +10,19 @@ app.use(cors())
 app.use(express.json())
 app.use(routes)
 
-app.listen(3333)
+let user = ''
+let pass = encodeURIComponent('')
+
+mongoose.connect(
+        `mongodb+srv://${user}:${pass}@cinfra-club.csmendp.mongodb.net/?retryWrites=true&w=majority`
+    )
+    .then(() => {
+
+        console.log("MongoDB successfully connected")
+        app.listen(3333)
+
+    }).catch((err) => {
+
+        console.log(err)
+
+    })
