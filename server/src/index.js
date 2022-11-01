2e63351411a787a996af5de01b2490c085c56929
Gabriel <gkqsj@cin.ufpe.br>
2022-11-01 18:50:31 -0300

    Fixed chords addition bug

--- server/src/index.js
+++ server/src/index.js
@@ -2,7 +2,6 @@ const express = require('express')
 const routes = require('./routes')
 const cors = require('cors')
 const mongoose = require('mongoose')
-const { urlencoded } = require('express')
 
 const app = express()
 
