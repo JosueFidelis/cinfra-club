aa9dd8dc5b46b4d7368d8895487b34c7e68d0cd3
Gabriel <gkqsj@cin.ufpe.br>
2022-11-02 19:00:22 -0300

    makes query accept genres

--- server/src/routes.js
+++ server/src/routes.js
@@ -4,15 +4,31 @@ const routes = express.Router()
 
 const Song = require('./models/Song')
 
-routes.get('/songs', async (req, res) => {
+routes.get('/songs', async(req, res) => {
     try {
         const chords = req.headers.chords.split(",")
-        const song = await Song.find({$expr: {$setIsSubset: ["$chords", chords]}})
-        
+        const genres = req.headers.genres
+        let song
+        if (genres !== undefined && genres.length > 0) {
+            song = await Song.find({
+                $expr: { $setIsSubset: ["$chords", chords] },
+                "genre": { "$in": [genres] }
+            })
+        } else {
+            song = await Song.find({
+                $expr: { $setIsSubset: ["$chords", chords] }
+            })
+        }
+
+        // const xong = await Song.find({
+        //     "genre": { "$in": [genres] },
+        //     "chords": { "$in": [chords] }
+        // })
+
         res.status(200).json(song)
     } catch (error) {
         console.log(error)
-        res.status(404).json({error: error})
+        res.status(404).json({ error: error })
     }
 })
 
