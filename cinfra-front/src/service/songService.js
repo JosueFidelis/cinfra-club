91858721fa82d1e789ab7d89d7dac894aa04ff5e
JosueFidelis <jfon@cin.ufpe.br>
2022-11-02 19:07:24 -0300

    add service to front

---
+++ cinfra-front/src/service/songService.js
@@ -0,0 +1,23 @@
+import axios from "axios";
+
+const apiClient = axios.create({
+  baseURL: `http://localhost:3000`,
+  withCredentials: false, // This is the default
+  headers: {
+    Accept: "application/json",
+    "Content-Type": "application/json",
+  },
+});
+
+export default {
+  getSongs(chords, genre) {
+    const config = {
+      headers: { chords, genre },
+    };
+
+    return apiClient.get("/songs", config);
+  },
+  getGenres() {
+    return apiClient.get("/genres");
+  },
+};
